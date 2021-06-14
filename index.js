eb504050016d80ba41bb0d0333ccb59ff2efbdaf
MaherAliwe <maheraliwe4@gmail.com>
2021-06-14 16:32:52 +0300

    step-5 Done

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 const port = 3000
 var time = new Date();
 var date = time.getHours() + ':' + time.getMinutes();
+const movies = [
+    { title: 'Jaws', year: 1975, rating: 8 },
+    { title: 'Avatar', year: 2009, rating: 7.8 },
+    { title: 'Brazil', year: 1985, rating: 8 },
+    { title: 'الإرهاب والكباب‎', year: 1992, rating: 6.2 }
+]
 
 app.get('/', (req, res) => {
     res.send('Ok')
@@ -21,11 +27,11 @@ app.get('/time', (req, res) => {
 })
 
 app.get('/hello/:name', (req, res) => {
-    res.send({ status: 200, message: "hello, " + req.params.name + "!" });
+    res.send({ status: 200, message: "hello " + req.params.name });
 })
 
 app.get('/hello/', (req, res) => {
-    res.send({ status: 200, message: "hello, !" });
+    res.send({ status: 200, message: "hello" });
 })
 
 app.get('/search', (req, res) => {
@@ -35,4 +41,23 @@ app.get('/search', (req, res) => {
         res.send({ status: 500, error: true, message: "you have to provide a search" });
     }
     res.send({ status: 200, message: "ok", data: search });
+})
+app.get('/movies/add', (req, res) => {
+    res.send({ status: 200, message: "ok", data: movies });
+})
+
+app.get('/movies/get', (req, res) => {
+    res.send({ status: 200, message: "ok", data: movies });
+})
+
+app.get('/movies/edit', (req, res) => {
+    res.send({ status: 200, message: "Ok", data: movies });
+})
+
+app.get('/movies/delete', (req, res) => {
+    res.send({ status: 200, message: "ok", data: movies });
+})
+
+app.get('/movies/read', (req, res) => {
+    res.send({ status: 200, message: "Ok", data: movies });
 })
