904c6d7c2f2ef4dd55805d4eb866942c5cc0efc6
MaherAliwe <maheraliwe4@gmail.com>
2021-06-14 15:34:00 +0300

    step-4 Done

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log(`Example app listening at http://localhost:3000`)
 })
 
 app.get('/test', (req, res) => {
@@ -18,4 +18,21 @@ app.get('/test', (req, res) => {
 
 app.get('/time', (req, res) => {
     res.send({ status: 200, message: date })
+})
+
+app.get('/hello/:name', (req, res) => {
+    res.send({ status: 200, message: "hello, " + req.params.name + "!" });
+})
+
+app.get('/hello/', (req, res) => {
+    res.send({ status: 200, message: "hello, !" });
+})
+
+app.get('/search', (req, res) => {
+    const search = req.query.s;
+    if (search == "") {
+        res.status(500);
+        res.send({ status: 500, error: true, message: "you have to provide a search" });
+    }
+    res.send({ status: 200, message: "ok", data: search });
 })
